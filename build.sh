b0d383c26ddedf9b31988828a6d1826c31781f0d
Evans Tucker <evans.tucker@gmail.com>
2020-03-28 14:56:02 -0700

    Initial release.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+export chart_org=$(echo "$1" | cut -d/ -f1)
+
+# The registry.gitlab.com/dedevsecops/workstation:4af7794f image has both Helm
+# 2 and Helm 3 in it, this `helm3` specifies that we're using Helm 3 to build
+# the image.
+
+#helm3 repo add unleash --username unleash --password "${CHARTMUSEUM_PASSWORD}" https://charts.unleash.org
+helm3 repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm3 repo update
+helm3 lint "${1}"
+helm3 dependency update "${1}"
+helm3 package "${1}"
+#helm3 push "${1}" "${chart_org}"
