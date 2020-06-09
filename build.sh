cf2ace8c4d3b9083dae3c260b433cc6f4f4c73d7
Evans Tucker <evans.tucker@gmail.com>
2020-06-08 17:21:07 -0700

    Switching to latest.

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -xeuo pipefail
 
 export chart_org=$(echo "$1" | cut -d/ -f1)
 
-# The registry.gitlab.com/dedevsecops/workstation:4af7794f image has both Helm
-# 2 and Helm 3 in it, this `helm3` specifies that we're using Helm 3 to build
-# the image.
+# The registry.gitlab.com/dedevsecops/workstation image has both Helm 2 and
+# Helm 3 in it, this `helm3` specifies that we're using Helm 3 to build the
+# image.
 
 #helm3 repo add unleash --username unleash --password "${CHARTMUSEUM_PASSWORD}" https://charts.unleash.org
 helm3 repo add stable https://kubernetes-charts.storage.googleapis.com/
