e3112af5cbdc8347bc14d5e5c48f77a3dc51f320
Evans Tucker <evans.tucker@gmail.com>
2020-09-30 21:21:34 -0700

    The upstream workstation image no longer has Helm 2, and `helm` is now Helm 3.

--- build.sh
+++ build.sh
@@ -4,14 +4,10 @@ set -xeuo pipefail
 
 export chart_org=$(echo "$1" | cut -d/ -f1)
 
-# The registry.gitlab.com/dedevsecops/workstation image has both Helm 2 and
-# Helm 3 in it, this `helm3` specifies that we're using Helm 3 to build the
-# image.
-
-#helm3 repo add unleash --username unleash --password "${CHARTMUSEUM_PASSWORD}" https://charts.unleash.org
-helm3 repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm3 repo update
-helm3 lint "${1}"
-helm3 dependency update "${1}"
-helm3 package "${1}"
-#helm3 push "${1}" "${chart_org}"
+#helm repo add unleash --username unleash --password "${CHARTMUSEUM_PASSWORD}" https://charts.unleash.org
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo update
+helm lint "${1}"
+helm dependency update "${1}"
+helm package "${1}"
+#helm push "${1}" "${chart_org}"
