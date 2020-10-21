2f0ba88e300464bfa1288cd62649831c7866ead3
Evans Tucker <evans.tucker@gmail.com>
2020-10-21 09:41:08 -0700

    Switch from https://kubernetes-charts.storage.googleapis.com to https://charts.helm.sh/stable

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -xeuo pipefail
 export chart_org=$(echo "$1" | cut -d/ -f1)
 
 #helm repo add unleash --username unleash --password "${CHARTMUSEUM_PASSWORD}" https://charts.unleash.org
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add stable https://charts.helm.sh/stable/
 helm repo update
 helm lint "${1}"
 helm dependency update "${1}"
