c583a4dc2064d1a5ce3ae202012716a220d82971
Christopher Kolstad <chriswk@getunleash.ai>
2023-03-16 13:23:54 +0100

    task: move from kubeval to kubeconform
    In the process I also spotted that we're not actually linting/validating
    all our schemas, since we run helm template without any values, it's run
    without ingress and hpa enabled, so this PR also adds values files which
    causes all template files to be exercised.

---
+++ .github/kubeconform.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# use kubeconform to validate helm generated kubernetes manifests
+#
+
+set -o errexit
+set -o pipefail
+
+echo "Running for K8S schema version: $KUBERNETES_VERSION with Kube conform: $KUBECONFORM_VERSION"
+# install kubeconform
+curl --silent --show-error --fail --location --output /tmp/kubeconform.tar.gz https://github.com/yannh/kubeconform/releases/download/"${KUBECONFORM_VERSION}"/kubeconform-linux-amd64.tar.gz
+sudo tar -C /usr/local/bin -xf /tmp/kubeconform.tar.gz kubeconform
+
+mkdir -p results
+echo "Adding bitnami repo so dependency building succeeds"
+helm repo add bitnami https://charts.bitnami.com/bitnami
+echo "Repo added"
+CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+for CHART_DIR in ${CHART_DIRS}; do
+  echo "helm dependency build..."
+  helm dependency build "${CHART_DIR}"
+
+  echo "kubeconforming ${CHART_DIR##charts/} chart ..."
+  helm template "${CHART_DIR}" -f ./.github/helmvalues/"${CHART_DIR##charts/}"/"${CHART_DIR##charts/}"-values.yaml | kubeconform -kubernetes-version "${KUBERNETES_VERSION}" --output=tap > results/"${CHART_DIR##charts/}"-"${KUBERNETES_VERSION}"-result.tap
+done
+
+exit 0
